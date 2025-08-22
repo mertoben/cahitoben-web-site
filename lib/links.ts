f3653864cd3b0cf0f1c44a123dca3fce994f7593
mertoben <mertoben@gmail.com>
2025-08-22 17:08:25 +0300

    Create links.ts

---
+++ lib/links.ts
@@ -0,0 +1,6 @@
+// lib/links.ts
+export const ARTIST_LINKS = {
+  spotify: 'https://open.spotify.com/intl-tr/artist/2O8jY0TevDFs7R9tzkh4SH',
+  apple: 'https://music.apple.com/tr/artist/cahit-oben/95145871',
+  youtube: 'https://www.youtube.com/channel/UCWiOnChcc4l8peYF7e0XXfw',
+};
